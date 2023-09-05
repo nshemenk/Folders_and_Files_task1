5d30946c19c9b7284708c219b9e8627caf1ca16b
Shemenko Natalia <nmeleshina@gmail.com>
2023-09-05 11:33:29 +0300

    mistakes correction of Task1 and v.1 of Task2

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class Main {
     public static void main(String[] args) {
@@ -18,24 +21,9 @@ public class Main {
             text.append(makeDir(path) + '\n');
         }
 
-        String[][] files = {
-                {"C:\\Users\\nmele\\Games\\src\\main",
-                        "C:\\Users\\nmele\\Games\\src\\main",
-                        "C:\\Users\\nmele\\Games\\temp"},
-                {"Main.java",
-                        "Utils.java",
-                        "temp.txt"}
-        };
-
-        String file;
-        String path;
-
-
-        for (int j = 0; j < files[0].length; j++) {
-            path = files[0][j];
-            file = files[1][j];
-            text.append(makeFile(path, file) + '\n');
-        }
+        text.append(makeFile("C:\\Users\\nmele\\Games\\src\\main", "Main.java") + '\n');
+        text.append(makeFile("C:\\Users\\nmele\\Games\\src\\main", "Utils.java") + '\n');
+        text.append(makeFile("C:\\Users\\nmele\\Games\\temp", "temp.txt") + '\n');
 
 
         File fileToWrite = new File("C:\\Users\\nmele\\Games\\temp", "temp.txt");
@@ -49,8 +37,33 @@ public class Main {
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
-    }
 
+        ArrayList<GameProgress> gamesProgress = new ArrayList<>();
+        gamesProgress.add(new GameProgress(94, 10, 2, 254.32));
+        gamesProgress.add(new GameProgress(58, 6, 3, 300.58));
+        gamesProgress.add(new GameProgress(33, 2, 4, 320.25));
+
+        ArrayList<String> filesPaths = new ArrayList<>();
+        filesPaths.add("C:\\Users\\nmele\\Games\\savegames\\save1.dat");
+        filesPaths.add("C:\\Users\\nmele\\Games\\savegames\\save2.dat");
+        filesPaths.add("C:\\Users\\nmele\\Games\\savegames\\save3.dat");
+
+        for (int i = 0; i < filesPaths.size(); i++) {
+            saveGame(filesPaths.get(i), gamesProgress.get(i));
+        }
+
+
+        String zipPath = "C:\\Users\\nmele\\Games\\savegames\\zip.zip";
+        zipFiles(zipPath, filesPaths);
+
+        for (String filePath : filesPaths) {
+            File fileForDelete = new File(filePath);
+            if (fileForDelete.delete()) {
+                System.out.println("Файл " + fileForDelete.getName() + " удален");
+            } else System.out.println("Файл " + fileForDelete.getName() + " не найден");
+        }
+
+    }
 
     public static String makeDir(String path) {
         File directory = new File(path);
@@ -71,6 +84,37 @@ public class Main {
             throw new RuntimeException(e);
         }
 
+    }
+
+    public static void saveGame(String path, GameProgress gameProgress) {
+        try (FileOutputStream fos = new FileOutputStream(path);
+             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            oos.writeObject(gameProgress);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public static void zipFiles(String zipPath, ArrayList<String> filesPaths) {
+
+        try (ZipOutputStream zout = new ZipOutputStream(new
+                FileOutputStream(zipPath))) {
+            for (String filePath : filesPaths) {
+                try (FileInputStream fis = new FileInputStream(filePath)) {
+                    File fileForZip = new File(filePath);
+                    ZipEntry entry = new ZipEntry(fileForZip.getName());
+                    zout.putNextEntry(entry);
+                    byte[] buffer = new byte[fis.available()];
+                    fis.read(buffer);
+                    zout.write(buffer);
+                    zout.closeEntry();
+                } catch (IOException ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 
 }
